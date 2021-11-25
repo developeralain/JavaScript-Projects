b1686903e03404f50a7fc5c5a79e2777f6a14e7b
developeralain <93736888+developeralain@users.noreply.github.com>
2021-11-25 10:13:38 -0500

    js work

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+var Dogs = "Pup Pups", Female = "Lacie", Male = "Louie", MalePup = "Orion", FemalePup = "Aquilla";
+
+var Dogs = Dogs.fontcolor("red");
+document.write(Dogs)
+
+var Female = Female.fontcolor("pink");
+document.write(Female)
+
+var Male = Male.fontcolor("grey");
+
+var MalePup = MalePup.fontcolor("yellow");
+
+var FemalePup = FemalePup.fontcolor("green");
+
+document.write(Female + Male)
+
